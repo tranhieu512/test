62a528e8f03758aaa8c78e02e3f5a4845719e996
tranhieu512 <tronghieu512@gmail.com>
2025-11-20 13:59:58 +0700

    Update process.py

--- scripts/process.py
+++ scripts/process.py
@@ -32,7 +32,7 @@ SOURCES = [
      "In The Box"),
     
     ("https://raw.githubusercontent.com/vuminhthanh12/vuminhthanh12/refs/heads/main/vmttv", 
-     r',HBO.*$', 
+     r'"Quốc Tế"', 
      "Nhóm Kênh Quốc Tế"),
     
 
