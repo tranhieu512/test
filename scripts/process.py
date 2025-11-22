8c4be2629ec2cefac5165031310455c4a0d78dae
tranhieu512 <tronghieu512@gmail.com>
2025-11-22 15:16:56 +0700

    Update process.py

--- scripts/process.py
+++ scripts/process.py
@@ -30,7 +30,6 @@ SOURCES = [
 
     ("https://raw.githubusercontent.com/vuminhthanh12/vuminhthanh12/refs/heads/main/vmttv", 
      r'"SCTV"', # Chỉ giữ SCTV
-     r'tvg-id="SCTV4K"', # Loại trừ SCTV15
      "Nhóm Kênh SCTV"),
 
     ("https://raw.githubusercontent.com/kupjta/iptv/main/kupjtv.m3u", 
